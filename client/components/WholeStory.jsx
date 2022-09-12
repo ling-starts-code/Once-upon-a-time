00d461ffa9d06326847a8e176308c00d65b3a598
Aaron <95986810+Aaront028@users.noreply.github.com>
2022-09-12 16:47:15 +1200

    add text to speech

--- client/components/WholeStory.jsx
+++ client/components/WholeStory.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import { SayButton } from 'react-say'
 
 function WholeStory() {
   const arr = useSelector((state) => state.paragraphies)
@@ -8,8 +9,19 @@ function WholeStory() {
   return (
     <>
       {arr.map((item) => {
-        return <p key={item.id}>{item.post}</p>
+        return (
+          <>
+            <SayButton
+              key={item.id}
+              onClick={(event) => console.log(event)}
+              speak={item.post}
+            >
+              {/* <p key={item.id}>{item.post}</p> */}
+            </SayButton>
+          </>
+        )
       })}
+      Tell me a story
     </>
   )
 }
